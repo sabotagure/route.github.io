9ac0a3f6b3ebd3b886c86e94a5f32732fd5861dd
sabotagure <157576891+sabotagure@users.noreply.github.com>
2024-05-25 08:58:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('fileInput').addEventListener('change', processCSV);
+    document.getElementById('calculateButton').addEventListener('click', processCSV);
 });
 
 function processCSV() {
